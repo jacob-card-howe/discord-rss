5cb8c70934fd6db1748dc62216c5339d95ad48f7
Jacob Howe <jhowe@NZXTSleeper-V1.localdomain>
2021-04-25 16:57:38 -0400

    Built in a loop to auto parse an RSS feed every 30 seconds for 3 hours at a time without any overlapping of the forloop

--- discord-rss/main.go
+++ discord-rss/main.go
@@ -6,8 +6,10 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/mmcdole/gofeed"
@@ -15,6 +17,7 @@ import (
 
 // Used to keep track of the latest RSS Update
 var messageArray []string
+var previousMessage []string
 
 // Used to keep track of recent Bot Messages
 var botMessageArray []string
@@ -24,6 +27,8 @@ var (
 	Token     string
 	Url       string
 	ChannelId string
+	// Can add this in later
+	// TickerTimer int
 )
 
 var message string
@@ -33,66 +38,150 @@ func init() {
 	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.StringVar(&Url, "u", "", "RSS Feed URL")
 	flag.StringVar(&ChannelId, "c", "", "Channel ID you want messages to post in")
+	// Can add this in later
+	// flag.IntVar(&TickerTimer, "timer", "", "Sets how long the auto parser will run for")
 	flag.Parse()
 }
 
+func GetCreationDate(ID string) (t time.Time, timeInRFC3339 string, err error) {
+	i, err := strconv.ParseInt(ID, 10, 64)
+	if err != nil {
+		return
+	}
+	timestamp := (i >> 22) + 1420070400000 // converts Snowflake ID to a unix timestamp
+	t = time.Unix(0, timestamp*1000000)
+	timeInRFC3339 = t.Format(time.RFC3339)
+	return
+}
+
+func messageCreated(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		log.Println("This bot posted the last message.")
+		return
+	}
+
+	if m.Content == "!status" {
+		_, err := s.ChannelMessageSend(ChannelId, "I'm running!\n\nIf you're not getting updates from your RSS feed, it's likely because it hasn't updated since your last message. Try again later!")
+		if err != nil {
+			log.Println("Error sending message:", err)
+		}
+	} else if len(previousMessage) > 0 {
+		// Compare timestamps of previous message and newest message
+		// ignore if latest message is too recent
+
+		_, timestamp, err := GetCreationDate(m.ID) // timestamp of message sent in Discord
+		if err != nil {
+			log.Println("Not a valid Snowflake ID:", err)
+		}
+
+		parseTimestamp, err := time.Parse(time.RFC3339, timestamp)
+		if err != nil {
+			log.Println("Error Parsing new message's timestamp:", err)
+		}
+
+		parseOldMessage, err := time.Parse(time.RFC3339, previousMessage[0])
+		if err != nil {
+			log.Println("Error Parsing old message's timestamp:", err)
+		}
+
+		diff := parseTimestamp.Sub(parseOldMessage)
+		log.Printf("These messages were posted %s apart", diff)
+
+		if diff.Hours() < 3 {
+			log.Println("The loop is still running! Skipping starting another loop.")
+		} else {
+			log.Println("Starting parser loop...")
+			ticker := time.NewTicker(30 * time.Second)
+			done := make(chan bool)
+
+			// An attempt to prevent doubling up on the loop below
+			previousMessage = append(previousMessage, timestamp)
+			copy(previousMessage[1:], previousMessage)
+			previousMessage[0] = timestamp
+
+			go func() {
+				for {
+					select {
+					case <-done:
+						return
+					case <-ticker.C:
+						sendUpdate(s)
+					}
+				}
+			}()
+
+			time.Sleep(3 * time.Hour)
+			ticker.Stop()
+			done <- true
+			fmt.Println("Stopped ticker.")
+		}
+	} else {
+		_, timestamp, err := GetCreationDate(m.ID)
+		if err != nil {
+			log.Println("Error getting Message Timestamp:", err)
+		} else {
+			// Send update to Channel
+			log.Println("Starting parser loop...")
+			ticker := time.NewTicker(30 * time.Second)
+			done := make(chan bool)
+
+			// An attempt to prevent doubling up on the loop below
+			previousMessage = append(previousMessage, timestamp)
+			copy(previousMessage[1:], previousMessage)
+			previousMessage[0] = timestamp
+
+			go func() {
+				for {
+					select {
+					case <-done:
+						return
+					case <-ticker.C:
+						sendUpdate(s)
+					}
+				}
+			}()
+
+			time.Sleep(3 * time.Hour)
+			ticker.Stop()
+			done <- true
+			fmt.Println("Stopped ticker.")
+		}
+	}
+}
+
 // Sends messageArray anytime a new message is sent to your Discord Server
-func sendMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
+func sendUpdate(s *discordgo.Session) {
+
 	// Clears the message array on every new message
 	log.Println("Clearing the messageArray...")
 	messageArray = nil
 
-	if m.Content == "!status" {
-		s.ChannelMessageSend(ChannelId, "I'm running! If you're not getting updates from your RSS feed, it's likely because it hasn't updated yet. Try again later!")
+	// Parses anytime a new message is detected in your Discord Server
+	feedParser := gofeed.NewParser()
+	log.Println("Parsing RSS Feed...")
+	//feed, err := feedParser.ParseURL("http://lorem-rss.herokuapp.com/feed?unit=second&interval=15") // Great RSS feed for testing :)
+	feed, err := feedParser.ParseURL(Url)
+	if err != nil {
+		log.Println("There was an error parsing the URL:", err)
+		return
 	}
 
-	if m.Author.ID == s.State.User.ID {
-		log.Println("This bot posted the last message. Not parsing again.")
-	} else {
-		// Parses anytime a new message is detected in your Discord Server
-		feedParser := gofeed.NewParser()
-		log.Println("Parsing RSS Feed...")
-		//feed, err := feedParser.ParseURL("http://lorem-rss.herokuapp.com/feed?length=10&unit=second&interval=60") // Great RSS feed for testing :)
-		feed, err := feedParser.ParseURL(Url)
-		if err != nil {
-			log.Println("There was an error parsing the URL:", err)
-			return
-		}
+	// Grabs last 5 RSS Items and appends them to messageMap (Discord Character Limits)
+	log.Println("Generating messageArray...")
+	for i := 0; i <= 4; i++ {
+		message = fmt.Sprintf("%s!\n%s\n", feed.Items[i].Title, feed.Items[i].Link)
+		messageArray = append(messageArray, message)
+	}
 
-		// Grabs last 5 RSS Items and appends them to messageMap (Discord Character Limits)
-		log.Println("Generating messageArray...")
-		for i := 0; i <= 4; i++ {
-			message = fmt.Sprintf("%s!\n%s\n", feed.Items[i].Title, feed.Items[i].Link)
-			messageArray = append(messageArray, message)
-		}
+	// Formats messageArray into one big message instead of sending 5 individual messages
+	// It's noisy if we don't do it this way, and also exceeds Discord's character limit / message rate limit
+	log.Println("Generating bigMessage...")
+	convertToStrings := fmt.Sprintf(strings.Join(messageArray, "\n"))
+	bigMessage := fmt.Sprintf("Here are the 5 latest RSS Feed Items:\n\n%v", convertToStrings)
 
-		// Formats messageArray into one big message instead of sending 5 individual messages
-		// It's noisy if we don't do it this way, and also exceeds Discord's character limit / message rate limit
-		log.Println("Generating bigMessage...")
-		convertToStrings := fmt.Sprintf(strings.Join(messageArray, "\n"))
-		bigMessage := fmt.Sprintf("Here are the 5 latest RSS Feed Items:\n\n%v", convertToStrings)
-
-		// Checks to see if there's a difference between messageArray & botMessageArray
-		if botMessageArray != nil && messageArray != nil {
-			if bigMessage != botMessageArray[0] {
-				log.Println("Sending message to Discord...")
-				_, err := s.ChannelMessageSend(ChannelId, bigMessage)
-				if err != nil {
-					log.Println("There was an error sending your message:", err)
-				} else {
-					log.Println("Your message:\n", bigMessage)
-				}
-
-				// Appends message to the front of botMessageArray
-				botMessageArray = append(botMessageArray, bigMessage)
-				copy(botMessageArray[1:], botMessageArray)
-				botMessageArray[0] = bigMessage
-			} else {
-				log.Println("I've posted this message recently, skipping new post.")
-			}
-		}
-
-		if botMessageArray == nil {
+	// Checks to see if there's a difference between messageArray & botMessageArray
+	if botMessageArray != nil && messageArray != nil {
+		if bigMessage != botMessageArray[0] {
 			log.Println("Sending message to Discord...")
 			_, err := s.ChannelMessageSend(ChannelId, bigMessage)
 			if err != nil {
@@ -105,14 +194,30 @@ func sendMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
 			botMessageArray = append(botMessageArray, bigMessage)
 			copy(botMessageArray[1:], botMessageArray)
 			botMessageArray[0] = bigMessage
+		} else {
+			log.Println("I've posted this message recently, skipping new post.")
+		}
+	}
 
+	if botMessageArray == nil {
+		log.Println("Sending message to Discord...")
+		_, err := s.ChannelMessageSend(ChannelId, bigMessage)
+		if err != nil {
+			log.Println("There was an error sending your message:", err)
+		} else {
+			log.Println("Your message:\n", bigMessage)
 		}
 
-		// Clears the botMessageArray if len(botMessageArray) > 1000
-		if len(botMessageArray) > 1000 {
-			botMessageArray = nil
-			return
-		}
+		// Appends message to the front of botMessageArray
+		botMessageArray = append(botMessageArray, bigMessage)
+		copy(botMessageArray[1:], botMessageArray)
+		botMessageArray[0] = bigMessage
+	}
+
+	// Clears the botMessageArray if len(botMessageArray) > 1000
+	if len(botMessageArray) > 1000 {
+		botMessageArray = nil
+		return
 	}
 }
 
@@ -125,7 +230,7 @@ func main() {
 	}
 
 	// Registers the sendMessage function as a callback for MessageCreate Events
-	dg.AddHandler(sendMessage)
+	dg.AddHandler(messageCreated)
 
 	// Sets the intentions of the bot, read through the docs
 	// This specifically says "I want this bot to deal with messages in channels (Guilds)"
